ed000ccc82f28e17eff5cd30e045f5a224697d8a
kristinang12 <kristinangalcober12@gmail.com>
2022-12-21 06:21:47 -0800

    Made controller name PagesController and change route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('homepage');
-});
+Route::get('/', [PagesController::class, 'index']);
